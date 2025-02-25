e8d76db29222310e38179cc2f81509d6aa61c513
mdmomen162 <mdmomenmiah162@gmail.com>
2025-02-26 01:11:39 +0600

    momen's commit

---
+++ lib/momen.dart
@@ -0,0 +1,19 @@
+// Library name: momen
+// Public class: momen2
+class momen2 {
+  // Public variable
+  String title = "Momen Library";
+
+  // Private variable
+  String _secretCode = "MOMEN123";
+
+  // Public method
+  void displayTitle() {
+    print("Library Title: $title");
+  }
+
+  // Private method
+  void _showSecret() {
+    print("Secret Code: $_secretCode");
+  }
+}
