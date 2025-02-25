617366a73b8fa2130ab4cdcd9bb9e216f96f4aef
MasudBinMazid <mamun15-5451@diu.edu.bd>
2025-02-25 20:49:45 +0600

    commitfrommasud

---
+++ lib/masud2.dart
@@ -0,0 +1,16 @@
+//masud is a library name
+//masud2 is a class name  and it is a public class
+class masud2 {
+  //public variable
+  var name = "Masud";
+  //private variable
+  var _name = "Masud";
+  //public method
+  void showName() {
+    print(name);
+  }
+  //private method
+  void _showName() {
+    print(_name);
+  }
+}
